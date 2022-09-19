14eb14bf0c1e2e8085b80a434e96b2de210fbc8e
Dorota Zelga <dorota.zelga@gmail.com>
2022-09-19 18:35:08 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,8 +19,7 @@ module.exports = {
     "plugins": [
         "react",
         "@typescript-eslint",
-        'prettier',
-        "unused-imports"
+        'prettier'
     ],
     "settings": {
         react: {
@@ -30,24 +29,18 @@ module.exports = {
     "rules": {
         'semi': ['error', 'always'],
         'quotes': ["error", "single"],
-        // "react/jsx-filename-extension": ["warn", { "extensions": [".tsx",] }],
+        "react/jsx-filename-extension": ["warn", { "extensions": [".tsx",] }],
         "react/jsx-uses-react": "off",
         "react/react-in-jsx-scope": "off",
-        // 'sort-imports': [
-        //     'error',
-        //     {
-        //       ignoreCase: false,
-        //       ignoreDeclarationSort: true, // don"t want to sort import lines, use eslint-plugin-import instead
-        //       ignoreMemberSort: false,
-        //       memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-        //       allowSeparatedGroups: true,
-        //     },
-        //   ],
-        // "@typescript-eslint/no-unused-vars": "off",
-        // "unused-imports/no-unused-imports": "error",
-        // "unused-imports/no-unused-vars": [
-        //     "error",
-        //     { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
-        // ]
+        'sort-imports': [
+            'error',
+            {
+                ignoreCase: false,
+                ignoreDeclarationSort: true,
+                ignoreMemberSort: false,
+                memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+                allowSeparatedGroups: true,
+            },
+        ],
     }
 }
